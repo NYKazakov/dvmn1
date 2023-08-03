9eaab07816e59f6021ead47100247958a0c10dda
nikokazakov <nikokazakov@ozon.ru>
2023-08-03 19:22:28 +0300

    fixes

--- lesson1.py
+++ lesson1.py
@@ -4,10 +4,10 @@ import requests
 def get_weather(location):
     base_url = 'https://wttr.in/'
     payload = {'nTqmM': '', 'lang': 'ru'}
-    url = base_url + location
+    url = f'{base_url}{location}'
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    print(response.text + "\n")
+    print(response.text)
 
 
 if __name__ == '__main__':
